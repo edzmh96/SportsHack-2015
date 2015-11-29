72f8237a4edf0d689033e2e2568283c1f00a89d7
John Vidal <joao.lvidal@gmail.com>
2015-11-29 02:53:43 -0800

    Fixes,add routes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var gameEvents = require('./routes/gameEvents')
+var gameEvent = require('./routes/gameEvent')
 var app = express();
 
 // view engine setup
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/gameEvents', gameEvents);
+app.use('/game-event', gameEvent);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/gameEvent.js
+++ routes/gameEvent.js
@@ -1,11 +1,13 @@
 /**
  * Created by jlvidal on 2015-11-29.
  */
+
+var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/looking', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+    res.send("OK");
 });
 
 module.exports = router;

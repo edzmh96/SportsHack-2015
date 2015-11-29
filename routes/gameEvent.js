6edc2376a27e5513b2a551514f33292441130c38
John Vidal <joao.lvidal@gmail.com>
2015-11-29 02:47:14 -0800

    Last Changes

--- routes/gameEvent.js
+++ routes/gameEvent.js
@@ -1,3 +1,11 @@
 /**
  * Created by jlvidal on 2015-11-29.
  */
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+module.exports = router;

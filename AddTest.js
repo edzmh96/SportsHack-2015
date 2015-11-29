aa561a4f1d8bfa63b28759827d8484f220481ed4
Abraham Lin <abraham_lin1996@berkeley.edu>
2015-11-29 04:14:33 -0800

    Getting things

--- AddTest.js
+++ AddTest.js
@@ -2,7 +2,7 @@ console.log("started");
 var api = require('./CFLdatabase');
 console.log("loaded");
 // Our test event_objects
-var event1 = {restaraunt_name : "Apple Bees", restaraunt_long : 12, restaraunt_lat : 20, capacity_of_restaraunt : 12, number_of_customers : 3, event_name : "Warriors View Party", image_url : "www.warriors.com", yelp_url : "www.yelp1.com"}
+var event1 = {restaraunt_name : "Jack Astor’s Bar & Grill", restaraunt_long : -79.949102, restaraunt_lat : 43.226337, capacity_of_restaraunt : 74, number_of_customers : 12, event_name : "Ottawa Red Blacks vs Edmonton Eskimos @ 3:00pm - 7:00pm, November 29th, 2015", image_url : "http://s3-media1.fl.yelpcdn.com/bphoto/gQLT87Mqzd7fcEjcc_AeGQ/o.jpg", yelp_url : "http://www.yelp.ca/biz/jack-astors-bar-and-grill-ancaster", game_id : 1}
 var event2 = {restaraunt_name : "Chilis", restaraunt_long : 32, restaraunt_lat : 69, capacity_of_restaraunt : 49, number_of_customers : 23, event_name : "Bulls View Party", image_url : "www.bulls.com", yelp_url : "www.yelp2.com"}
 var event3 = {restaraunt_name : "Tim Hortons", restaraunt_long : 83, restaraunt_lat : 47, capacity_of_restaraunt : 22, number_of_customers : 11, event_name : "Clippers View Party", image_url : "www.clippers.com", yelp_url : "www.yelp3.com"}
 var event4 = {restaraunt_name : "Gypsy", restaraunt_long : 11, restaraunt_lat : 33, capacity_of_restaraunt : 87, number_of_customers : 11, event_name : "Hornets View Party", image_url : "www.hornets.com", yelp_url : "www.yelp4.com"}

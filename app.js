7b08beb5321ab6be71a9c6c61a4db4d1140b5a07
John Vidal <joao.lvidal@gmail.com>
2015-11-29 02:49:47 -0800

    New route

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
+var gameEvents = require('./routes/gameEvents')
 var app = express();
 
 // view engine setup
@@ -24,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/gameEvents', gameEvents);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
